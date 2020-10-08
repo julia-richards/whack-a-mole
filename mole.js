8b9c89375ee07cc827a19dc39421ac124849d848
julia-richards <julia.m.richards@gmail.com>
2020-10-08 11:23:38 -0600

    completed game logic

--- mole.js
+++ mole.js
@@ -1,30 +1,38 @@
 // Write your JS here.
 window.addEventListener("DOMContentLoaded", () => {
-	const heads = Array.from(document.querySelectorAll(".wgs__mole-head"));
-
 	function initialize() {
+		const heads = Array.from(document.querySelectorAll(".wgs__mole-head"));
 		heads.forEach((mole) => {
-      mole.classList.add("wgs__mole-head--hidden");
-      mole.addEventListener('click', e => {
-        mole.classList.add("wgs__mole-head--hidden");
-      });
+			mole.classList.add("wgs__mole-head--hidden");
+			mole.addEventListener("click", (e) => {
+				mole.classList.add("wgs__mole-head--hidden");
+				mole.classList.add("wgs__mole-head--whacked");
+			});
 		});
 	}
 	initialize();
 
 	function popUpRandomMole() {
-    const num = Math.floor(Math.random() * Math.floor(8));
+		const heads = Array.from(
+			document.querySelectorAll(
+				".wgs__mole-head:not(.wgs__mole-head--whacked)"
+			)
+		);
+		if (!heads.length) {
+			alert("You won!!");
+			return;
+		}
+		const num = Math.floor(Math.random() * Math.floor(heads.length));
+		heads[num].classList.remove("wgs__mole-head--hidden");
 
-    heads[num].classList.remove("wgs__mole-head--hidden");
+		setTimeout(hideMole, 1000, heads[num]);
+	}
 
-    setTimeout(hideMole, 1000, heads[num]);
-  }
+	function hideMole(mole) {
+		mole.classList.add("wgs__mole-head--hidden");
 
-  function hideMole(mole) {
-    mole.classList.add("wgs__mole-head--hidden");
+		setTimeout(popUpRandomMole, 1000);
+	}
 
-    setTimeout(popUpRandomMole, 1000);
-  }
-
-  setTimeout(popUpRandomMole, 0);
+	setTimeout(popUpRandomMole, 0);
 });
