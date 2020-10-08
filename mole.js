2d6b9b86a5bafaccd59b6a4ac45295aaf959c9e7
julia-richards <julia.m.richards@gmail.com>
2020-10-08 10:54:13 -0600

    added grid format to mole board, set all to hidden to start

--- mole.js
+++ mole.js
@@ -1,1 +1,22 @@
 // Write your JS here.
+window.addEventListener("DOMContentLoaded", () => {
+	//   setInterval(() => {
+	//     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+	//     for (let moleHead of moleHeads) {
+	//       moleHead.classList.toggle('wgs__mole-head--hidden');
+	//     }
+	//   }, 1000);
+
+	const heads = document.querySelectorAll(".wgs__mole-head");
+
+	function setHidden() {
+		heads.forEach((mole) => {
+			mole.classList.add("wgs__mole-head--hidden");
+		});
+	}
+	setHidden();
+
+	function popUpRandomMole() {
+		const num = Math.floor(Math.random() * Math.floor(8));
+	}
+});
